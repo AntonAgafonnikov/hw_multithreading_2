61561057d29988c3dbc0348be19e35422f5587e2
Anton Agafonnikov <feograf@mail.ru>
2023-04-23 21:44:36 +0300

    Add branch 'freqlog'

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -10,7 +10,32 @@ public class Main {
     private static final char CHARACTER_TO_SEARCH = 'R';
 
     public static void main(String[] args) throws InterruptedException {
+        long startTs = System.currentTimeMillis();
         List<Thread> threads = new ArrayList<>();
+
+        Runnable taskCurrentMaximum = () -> {
+            int maximumFrequency = 0;
+            int currentMaximum = 0;
+            while (!Thread.interrupted()) {
+                synchronized (sizeToFreq) {
+                    try {
+                        sizeToFreq.wait();
+                    } catch (InterruptedException e) {
+                        return;
+                    }
+
+                    for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
+                        if (entry.getValue() > currentMaximum) {
+                            currentMaximum = entry.getValue();
+                            maximumFrequency = entry.getKey();
+                        }
+                    }
+                    System.out.println("Текущий лидер частот: " + maximumFrequency);
+                }
+            }
+        };
+        Thread threadCurrentMaximum = new Thread(taskCurrentMaximum);
+        threadCurrentMaximum.start();
 
         for (int i = 0; i < AMOUNT_ROUTES; i++) {
             Runnable task = () -> {
@@ -20,7 +45,7 @@ public class Main {
                 int numberCoincidences = (int) route.chars()
                         .filter(c -> (c == CHARACTER_TO_SEARCH))
                         .count();
-
+                // Заполняем Map
                 synchronized (sizeToFreq) {
                     if (sizeToFreq.containsKey(numberCoincidences)) {
                         int lineCounter = sizeToFreq.get(numberCoincidences) + 1;
@@ -28,18 +53,23 @@ public class Main {
                     } else {
                         sizeToFreq.put(numberCoincidences, 1);
                     }
+                    System.out.println(route + " -> " + numberCoincidences);
+                    sizeToFreq.notify();
                 }
-                System.out.println(route + " -> " + numberCoincidences);
             };
             // Создаём новый поток для каждой строки, сохраняем его в список и стартуем
             Thread thread = new Thread(task);
             threads.add(thread);
             thread.start();
+            thread.join();
         }
 
+        // Зависаем, ждём когда поток, объект которого лежит в thread, завершится
         for (Thread thread : threads) {
-            thread.join(); // Зависаем, ждём когда поток, объект которого лежит в thread, завершится
+            thread.join();
         }
+        // Прерываем поток
+        threadCurrentMaximum.interrupt();
 
         // Находим наиболее частое количество повторов и сколько маршрутов содержит данное число повторов
         int meetingFrequency = 0;
@@ -52,16 +82,20 @@ public class Main {
                 maximumNumberRepetitions = value;
             }
         }
+
         // Убираем найденную пару из Map для дальнейшего корректного вывода остальных повторов в цикле
         sizeToFreq.remove(meetingFrequency);
 
         System.out.println("\nСамое частое количество повторений " + meetingFrequency +
                 " (встретилось " + maximumNumberRepetitions + " раз)" +
                 "\nДругие размеры:");
-
         for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
             System.out.println("- " + entry.getKey() + " (" + entry.getValue() + " раз)");
         }
+
+        // Время работы программы
+        long endTs = System.currentTimeMillis();
+        System.out.println("Time: " + (endTs - startTs) + "ms");
     }
 
     public static String generateRoute(String letters, int length) {
