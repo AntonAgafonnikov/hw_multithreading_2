b193e5e2dc5debdb722b37d9413eb695368bc74e
Anton Agafonnikov <feograf@mail.ru>
2023-04-24 20:40:55 +0300

    Cosmetic changes. Removed unnecessary.

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -10,9 +10,60 @@ public class Main {
     private static final char CHARACTER_TO_SEARCH = 'R';
 
     public static void main(String[] args) throws InterruptedException {
-        long startTs = System.currentTimeMillis();
         List<Thread> threads = new ArrayList<>();
 
+        // Создаём поток для вывода текущего максимума частот и стартуем его
+        Thread threadCurrentMaximum = currentMaximum();
+        threadCurrentMaximum.start();
+
+        for (int i = 0; i < AMOUNT_ROUTES; i++) {
+            // Создаём новый поток для обработки каждого маршрута, сохраняем его в список и стартуем
+            Thread threadCountingRouteFrequencies = countingRouteFrequencies();
+            threads.add(threadCountingRouteFrequencies);
+            threadCountingRouteFrequencies.start();
+            threadCountingRouteFrequencies.join();
+        }
+
+        // Зависаем, ждём когда поток, объект которого лежит в thread, завершится
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        // Прерываем поток вывода текущего максимума
+        threadCurrentMaximum.interrupt();
+    }
+
+    public static String generateRoute(String letters, int length) {
+        Random random = new Random();
+        StringBuilder route = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            route.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return route.toString();
+    }
+
+    private static Thread countingRouteFrequencies(){
+        Runnable taskCountingRouteFrequencies = () -> {
+            // Генерация маршрута
+            String route = generateRoute(ROUTE_GENERATION_TEXT, ROUTE_LENGTH);
+            // Подсчёт поворотов направо в маршруте
+            int numberCoincidences = (int) route.chars()
+                    .filter(c -> (c == CHARACTER_TO_SEARCH))
+                    .count();
+            // Заполняем Map
+            synchronized (sizeToFreq) {
+                if (sizeToFreq.containsKey(numberCoincidences)) {
+                    int lineCounter = sizeToFreq.get(numberCoincidences) + 1;
+                    sizeToFreq.put(numberCoincidences, lineCounter);
+                } else {
+                    sizeToFreq.put(numberCoincidences, 1);
+                }
+                sizeToFreq.notify();
+            }
+        };
+        return new Thread(taskCountingRouteFrequencies);
+    }
+
+    private static Thread currentMaximum(){
         Runnable taskCurrentMaximum = () -> {
             int maximumFrequency = 0;
             int currentMaximum = 0;
@@ -34,76 +85,6 @@ public class Main {
                 }
             }
         };
-        Thread threadCurrentMaximum = new Thread(taskCurrentMaximum);
-        threadCurrentMaximum.start();
-
-        for (int i = 0; i < AMOUNT_ROUTES; i++) {
-            Runnable task = () -> {
-                // Генерация маршрута
-                String route = generateRoute(ROUTE_GENERATION_TEXT, ROUTE_LENGTH);
-                // Подсчёт поворотов направо в маршруте
-                int numberCoincidences = (int) route.chars()
-                        .filter(c -> (c == CHARACTER_TO_SEARCH))
-                        .count();
-                // Заполняем Map
-                synchronized (sizeToFreq) {
-                    if (sizeToFreq.containsKey(numberCoincidences)) {
-                        int lineCounter = sizeToFreq.get(numberCoincidences) + 1;
-                        sizeToFreq.put(numberCoincidences, lineCounter);
-                    } else {
-                        sizeToFreq.put(numberCoincidences, 1);
-                    }
-                    System.out.println(route + " -> " + numberCoincidences);
-                    sizeToFreq.notify();
-                }
-            };
-            // Создаём новый поток для каждой строки, сохраняем его в список и стартуем
-            Thread thread = new Thread(task);
-            threads.add(thread);
-            thread.start();
-            thread.join();
-        }
-
-        // Зависаем, ждём когда поток, объект которого лежит в thread, завершится
-        for (Thread thread : threads) {
-            thread.join();
-        }
-        // Прерываем поток
-        threadCurrentMaximum.interrupt();
-
-        // Находим наиболее частое количество повторов и сколько маршрутов содержит данное число повторов
-        int meetingFrequency = 0;
-        int maximumNumberRepetitions = 0;
-        for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
-            int key = entry.getKey();
-            int value = entry.getValue();
-            if (entry.getValue() > maximumNumberRepetitions) {
-                meetingFrequency = key;
-                maximumNumberRepetitions = value;
-            }
-        }
-
-        // Убираем найденную пару из Map для дальнейшего корректного вывода остальных повторов в цикле
-        sizeToFreq.remove(meetingFrequency);
-
-        System.out.println("\nСамое частое количество повторений " + meetingFrequency +
-                " (встретилось " + maximumNumberRepetitions + " раз)" +
-                "\nДругие размеры:");
-        for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
-            System.out.println("- " + entry.getKey() + " (" + entry.getValue() + " раз)");
-        }
-
-        // Время работы программы
-        long endTs = System.currentTimeMillis();
-        System.out.println("Time: " + (endTs - startTs) + "ms");
-    }
-
-    public static String generateRoute(String letters, int length) {
-        Random random = new Random();
-        StringBuilder route = new StringBuilder();
-        for (int i = 0; i < length; i++) {
-            route.append(letters.charAt(random.nextInt(letters.length())));
-        }
-        return route.toString();
+        return new Thread(taskCurrentMaximum);
     }
 }
