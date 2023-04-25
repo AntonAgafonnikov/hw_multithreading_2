7d8f33e43eef2593c39116e1c04206f8ed9c7049
Anton Agafonnikov <feograf@mail.ru>
2023-04-25 12:28:34 +0300

    Cosmetic changes. Removal of Map filling in a separate method.

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -8,53 +8,36 @@ public class Main {
     private static final int ROUTE_LENGTH = 100;
     private static final String ROUTE_GENERATION_TEXT = "RLRFR";
     private static final char CHARACTER_TO_SEARCH = 'R';
+    private static int maxMeetingFrequency = 0;
+    private static int maximumNumberRepetitions = 0;
 
     public static void main(String[] args) throws InterruptedException {
         List<Thread> threads = new ArrayList<>();
 
         for (int i = 0; i < AMOUNT_ROUTES; i++) {
-            Runnable task = () -> {
-                // Генерация маршрута
-                String route = generateRoute(ROUTE_GENERATION_TEXT, ROUTE_LENGTH);
-                // Подсчёт поворотов направо в маршруте
-                int numberCoincidences = (int) route.chars()
-                        .filter(c -> (c == CHARACTER_TO_SEARCH))
-                        .count();
-
-                synchronized (sizeToFreq) {
-                    if (sizeToFreq.containsKey(numberCoincidences)) {
-                        int lineCounter = sizeToFreq.get(numberCoincidences) + 1;
-                        sizeToFreq.put(numberCoincidences, lineCounter);
-                    } else {
-                        sizeToFreq.put(numberCoincidences, 1);
-                    }
-                }
-                System.out.println(route + " -> " + numberCoincidences);
-            };
-            // Создаём новый поток для каждой строки, сохраняем его в список и стартуем
-            Thread thread = new Thread(task);
-            threads.add(thread);
-            thread.start();
+            // Создаём новый поток для обработки каждого маршрута, сохраняем его в список и стартуем
+            Thread threadCountingRouteFrequencies = countingRouteFrequencies();
+            threads.add(threadCountingRouteFrequencies);
+            threadCountingRouteFrequencies.start();
         }
 
+        // Зависаем, ждём когда поток, объект которого лежит в thread, завершится
         for (Thread thread : threads) {
-            thread.join(); // Зависаем, ждём когда поток, объект которого лежит в thread, завершится
+            thread.join();
         }
 
-        int meetingFrequency = 0;
-        int maximumNumberRepetitions = 0;
-
+        // Находим максимальную частоту повторов, сохагяем удаляем её по ключу
         for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
             int key = entry.getKey();
             int value = entry.getValue();
             if (entry.getValue() > maximumNumberRepetitions) {
-                meetingFrequency = key;
+                maxMeetingFrequency = key;
                 maximumNumberRepetitions = value;
             }
         }
-        sizeToFreq.remove(meetingFrequency);
+        sizeToFreq.remove(maxMeetingFrequency);
 
-        System.out.println("\nСамое частое количество повторений " + meetingFrequency +
+        System.out.println("\nСамое частое количество повторений " + maxMeetingFrequency +
                 " (встретилось " + maximumNumberRepetitions + " раз)");
 
         for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
@@ -69,5 +52,27 @@ public class Main {
             route.append(letters.charAt(random.nextInt(letters.length())));
         }
         return route.toString();
+    }
+
+    private static Thread countingRouteFrequencies() {
+        Runnable taskCountingRouteFrequencies = () -> {
+            // Генерация маршрута
+            String route = generateRoute(ROUTE_GENERATION_TEXT, ROUTE_LENGTH);
+            // Подсчёт поворотов направо в маршруте
+            int numberCoincidences = (int) route.chars()
+                    .filter(c -> (c == CHARACTER_TO_SEARCH))
+                    .count();
+            // Заполняем Map
+            synchronized (sizeToFreq) {
+                if (sizeToFreq.containsKey(numberCoincidences)) {
+                    int lineCounter = sizeToFreq.get(numberCoincidences) + 1;
+                    sizeToFreq.put(numberCoincidences, lineCounter);
+                } else {
+                    sizeToFreq.put(numberCoincidences, 1);
+                }
+                sizeToFreq.notify();
+            }
+        };
+        return new Thread(taskCountingRouteFrequencies);
     }
 }
